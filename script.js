12e6620fd611e97d073e0b00a593e1e35db6f8f5
NorayrMamulyan <68728687+NorayrMamulyan@users.noreply.github.com>
2020-08-10 08:28:25 +0400

    responsive line

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+    var click = 0;
+
     function func() {
         if (window.innerHeight > 600) {
             window.innerHeight = 600;
@@ -14,19 +16,10 @@
             blockArray[count] = ctrNodes[i];
             count++;
         }
-        //Reducing the style of previus click 
-        for (let val of blockArray) {
-            if (val.style.opacity = "0") {
-                val.style.opacity = "1";
-                val.style.transition = "1.2s";
-            }
-        }
         //Declearing global variables
-        var transition = (Math.random() * 1.2);
-        var animation = Math.random() * (innerHeight - (450));
-        //Giving style to random child of #container
-        var blockStyle = blockArray[Math.floor(Math.random() * blockArray.length)];
-        blockStyle.style.opacity = "0";
+        var transition = (Math.random() * 3) + 1;
+        var animation = Math.random(4) * (600 - 132) /*((Math.random() * (innerHeight - (450))) + 100)*/ ;
+        ctr[1].style.left = container2Left + 'px';
         //Giving random position for #container in vertical
         ctr[0].style.top = animation + "px";
         ctr[1].style.top = animation / 2 + "px";
@@ -34,15 +27,112 @@
         ctr[0].style.transitionDuration = transition + "s";
         ctr[1].style.transitionDuration = transition + "s";
 
-
         //line
         var line = document.getElementById('line');
-        line.style.top = animation + "px";
-        line.style.transitionDuration = transition + "s";
-        line.style.transform = "skew(0deg," + (-animation / 8) + "deg)";
-        if (animation > innerHeight / 2 - 136) {
-            line.style.top = animation - (animation / 8) + "px";
-        } else if (animation < innerHeight / 2 - 136) {
-            line.style.top = animation - 30 + "px";
+
+
+        //rewriting
+
+
+
+        //tuning
+        //placeholders
+        var img = document.getElementsByTagName('img');
+        var title = document.getElementsByTagName('h4');
+        var par = document.getElementsByTagName('p');
+        var containerWidth = 200;
+        var container2Left = 400;
+        var container1Height = 220;
+        var container1Left = 0;
+        var container1Top = animation;
+        var container2Top = animation / 2;
+
+        if (click < 2) {
+            click++;
+        } else { click = 1 };
+
+        //at first click
+        if (click == 1) {
+            //place changing at first
+            container2Left += Math.random() * 38;
+            container1Left += Math.random() * 40;
+            //for first cont
+            img[0].classList.add('imgBot');
+            ctr[0].style.height = container1Height + 'px';
+            ctr[0].style.width = containerWidth + 'px';
+            ctr[0].style.left = container1Left + 'px';
+            title[0].style.fontSize = 20 + 'px';
+            title[0].style.color = 'black';
+            par[0].style.fontSize = 18 + 'px';
+
+            //for second cont
+            img[1].classList.add('imgBot');
+            ctr[1].style.height = container1Height + 'px';
+            ctr[1].style.width = containerWidth + 'px';
+            ctr[1].style.left = container2Left + 'px';
+            title[1].style.fontSize = 20 + 'px';
+            title[1].style.color = 'black';
+            par[1].style.fontSize = 18 + 'px';
+
+
+            //line
+            var x1 = containerWidth + container1Left + 7, //additional 7 for paddingMargin
+                x2 = container2Left + 2, //additional 2 for paddingMargin
+                y1 = container1Top + (container1Height / 2),
+                y2 = container2Top + (container1Height / 2);
+            line.style.width = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)) + 'px';
+            line.style.left = ((x1 + x2) / 2) - (Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)) / 2) + "px";
+            line.style.top = ((y1 + y2) / 2) + "px";
+            let slopeInRadian = Math.atan2(y1 - y2, x1 - x2);
+            line.style.transform = `rotate(${(slopeInRadian * 180) / Math.PI}deg)`;
+            document.getElementById('parRotate').classList.add('rotating');
+            //line transDurat
+            line.style.transitionDuration = transition + "s";
         }
+        //at second click
+        if (click == 2) {
+            //place changing at second 
+            container1Height -= 47;
+            container2Left += Math.random() * 118;
+            container1Left += Math.random() * 40;
+            //for first cont
+            img[0].classList.remove('imgBot');
+            img[0].style.height = 113 + 'px';
+            ctr[0].style.height = container1Height + 'px';
+            ctr[0].style.left = container1Left + 'px';
+            title[0].style.color = '#B50744';
+
+            //for second cont
+            img[1].classList.remove('imgBot');
+            img[1].style.height = 113 + 'px';
+            ctr[1].style.height = container1Height + 'px';
+            ctr[1].style.left = container2Left + 'px';
+            title[1].style.color = '#B50744';
+
+            //line         
+            var x1 = containerWidth + container1Left + 7, //additional 7 for paddingMargin
+                x2 = container2Left + 2, //additional 2 for paddingMargin
+                y1 = container1Top + (container1Height / 2),
+                y2 = container2Top + (container1Height / 2);
+            line.style.width = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)) + 'px';
+            line.style.left = ((x1 + x2) / 2) - (Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)) / 2) + "px";
+            line.style.top = ((y1 + y2) / 2) + "px";
+            let slopeInRadian = Math.atan2(y1 - y2, x1 - x2);
+            line.style.transform = `rotate(${(slopeInRadian * 180) / Math.PI}deg)`;
+            document.getElementById('parRotate').classList.add('rotating');
+            //transDurat
+            line.style.transitionDuration = transition + "s";
+
+        }
+        /*
+                var x1 = 50,
+                    x2 = 100,
+                    y1 = 50,
+                    y2 = 100;
+                var check = document.getElementById('check');
+                check.style.width = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)) + 'px';
+                check.style.top = (y1 + y2) / 2 + 'px';
+                check.style.left = (x1 + x2) / 2 - (Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2))) / 2 + 'px';
+                var radian = Math.atan2(y2 - y1, x2 - x1);
+                check.style.transform = `rotate(${radian * 180/ Math.PI}deg)`;*/
     }
